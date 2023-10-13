8a68a38067df224ff8c2ec6844c1aa1031ad372b
mickeydarrenlau <darrenwjlau@gmail.com>
2023-10-13 15:52:59 +0800

    Almost Done

--- default.py
+++ default.py
@@ -2,6 +2,3 @@ speed = 0.8
 slow_speed = 0.5
 turn_speed = 0.7
 wait_time = 1
-
-#21/11
-#5/355
--- steps/fith.py
+++ steps/fith.py
@@ -33,8 +33,8 @@ class fith:
 
             time.sleep(wait_time)
 
-            motor1.throttle = turn_speed
+            motor2.throttle = turn_speed
             time.sleep(0.8)
-            motor1.throttle = 0.0
+            motor2.throttle = 0.0
             
             time.sleep(wait_time)
